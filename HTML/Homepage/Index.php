eaa539e15ffd0f810a76f91f364661d0cb70270c
Sjunsei <71812546+Sjunsei@users.noreply.github.com>
2022-03-16 13:14:41 +0400

    Foundation_Code
    - Added Basic HTML Footer & Header

--- HTML/Homepage/Index.php
+++ HTML/Homepage/Index.php
@@ -7,6 +7,43 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <!-- Will be copy pasted under Includes Header-->
+    <header>
+        <nav>
+            <div> settings icon</div>
+                <div> 
+                    <ul>
+                        <li>Home</li>
+                        <li>Shop</li>
+                        <label> Website icon </label>
+                        <li>Blog</li>
+                        <li>About</li>
+                    </ul>
+                </div>
+            <div>
+                <label for=""><button>Profile Icon</button></label>
+                <label for=""><button>Shopping Cart Icon</button></label>
+            </div>
+        </nav>
+    </header>
+
+    <footer>
+        <div>
+            <h5> Follow Us </h5>
+            <div>
+                <label for=""><button>Airplane Icon</button></label>
+                <label for=""><button>Like</button></label>
+                <label for=""><button>Share</button></label>
+            </div>
+        </div>
+        <div>
+            <button> SUBSCRIBE FOR MORE REWARDS </button>
+        </div>
+        <div>
+            <h5> CONTACT US </h5>
+            <h6> 050 777 7777notarealnumber </h6>
+            <h6>MiniSize@Gmail.com</h6>
+        </div>
+    </footer>
 </body>
 </html>
