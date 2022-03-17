f92ac11ae8f8b1ef3e1d5a6ddaf89d0c163a6c83
akickandopen <akilah.lapure@gmail.com>
2022-03-17 12:22:39 +0400

    add bootstrap style link

--- HTML/Homepage/Index.php
+++ HTML/Homepage/Index.php
@@ -5,6 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <!-- bootstrap link -->
+    <link rel="stylesheet" href="../../styles/main.css">
+    
 </head>
 <body>
     <!-- Will be copy pasted under Includes Header-->
