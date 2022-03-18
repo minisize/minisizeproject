22b5a6d79b9f2efa63572fe1b4bda05612dfddf1
Sjunsei <71812546+Sjunsei@users.noreply.github.com>
2022-03-19 00:20:06 +0400

    Foundation Coding
    - Structured the HTML of minisize Homepage
    
    - Added Classes and formats to the Homepage
    
    - Created a CSS file for Homepage

--- HTML/Homepage/Index.php
+++ HTML/Homepage/Index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Minisize </title>
 
-    <!-- SCSS Links -->
+    <!-- CSS Links -->
     <link rel="stylesheet" href="../../CSS/Includes/header_n_footer.css">
 
     <!-- bootstrap link -->
@@ -22,6 +22,93 @@
 
     <!-- Enter Main Content Here-->
 
+    <main>
+        <div class="Homepage_Product_1">
+
+            <label for=""> Picture of Hand </label>
+            <div>
+                <h1> MiniSize </h1>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+            </div>
+
+        </div>
+        <div>
+            <h3> BEST SELLERS </h3>
+
+            <div class="Product_Wrapper">
+                <div class="Product_Item_1">
+                <img src="#">
+                <h6> Item </h6>   
+                </div>
+                <div class="Product_Item_1">
+                <img src="#">
+                <h6> Item </h6>   
+                </div>
+                <div class="Product_Item_1">
+                <img src="#">
+                <h6> Item </h6>   
+                </div>
+                <div class="Product_Item_1">
+                <img src="#">
+                <h6> Item </h6>   
+                </div>
+                <div class="Product_Item_1">
+                <img src="#">
+                <h6> Item </h6>   
+                </div>
+                
+            </div>
+            
+        </div>
+        <div class="Main_Content">
+            <div class="Container1">
+                <img src="#">
+                <div class="Container1_SubContent">
+                    <h5> Meet our Bundles! </h5>
+                    <p>Meet our bundle! We provide small set of skincare products for one time use for our customers.</p>
+                    <button> View all </button>
+                </div>
+            </div>
+            <div class="Container2">
+                <div class="Container1_SubContent">
+                    <h5>Full-size products!</h5>
+                    <p>Full Sized products are available too! make sure to get a subscription in order to get discounts for the produts!</p>
+                    <button> View all </button>
+                </div>
+                <img src="#">
+            </div>
+        </div>
+        <div class="Sub_Content">
+            <h4> Find the right products for your skin type! </h4>
+            <div class="box1">
+                <button> 
+                    <h2> Oily </h2>
+                </button>
+                
+            </div>
+            <div class="box2">
+                <button> 
+                    <h2> Dry </h2>
+                </button>
+                
+            </div>
+            <div class="box3">
+                <button> 
+                    <h2> Sensitive </h2>
+                </button>
+                
+            </div>
+            <div class="box4">
+                <button> 
+                    <h2> Combination </h2>
+                </button>
+                
+            </div>
+        </div>
+    </main>
 
     <!-- Default Footer Paste -->
     <?php
