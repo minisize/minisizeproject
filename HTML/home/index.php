130c81349d99390be65430cf4a22afb8aec01f52
Sjunsei <71812546+Sjunsei@users.noreply.github.com>
2022-03-19 15:42:00 +0400

    Foundational_Coding
    - Modified Footer and Header file
    - Moved Head and end tags into Footer and Header file
    - NEEDS TO UPDATE FOR PROPER PATHFINDING

--- HTML/home/index.php
+++ HTML/home/index.php
@@ -1,24 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Minisize </title>
-
-    <!-- CSS Links -->
-    <link rel="stylesheet" href="../../CSS/Includes/header_n_footer.css">
-
-    <!-- bootstrap link -->
-    <link rel="stylesheet" href="../../styles/main.css">
-
-    
-</head>
-<body>
     <!-- Default Header Paste-->
     <?php
-        include_once '../Includes/Header.php'
+        include_once ''
     ?>
+
 
     <!-- Enter Main Content Here-->
 
@@ -114,5 +98,3 @@
     <?php
         include_once '../Includes/Footer.php'
     ?>
-</body>
-</html>
---
+++ HTML/include/footer.php
@@ -0,0 +1,24 @@
+<footer>
+<div class="Footer_Wrap Side_Footer">
+    <h5> Follow Us </h5>
+    <div class="Footer_Sub_Wrap">
+        <label for=""><button>Airplane Icon</button></label>
+        <label for=""><button>Like</button></label>
+        <label for=""><button>Share</button></label>
+    </div>
+</div>
+<div class="Footer_Wrap Main_Footer">
+    <button> SUBSCRIBE FOR MORE REWARDS </button>
+    <label> Website icon </label>
+</div>
+<div class="Footer_Wrap Side_Footer">
+    <h5> CONTACT US </h5>
+    <h6> 050 777 7777notarealnumber </h6>
+    <h6> MiniSize@Gmail.com </h6>
+</div>
+</footer>
+
+<!-- DO NOT REMOVE OR MODIFY PAST THIS -->
+
+</body>
+</html>
