da5b7aeb392938d33364fb3462d6974524cff664
KylaJaycee <kylajaycee28@gmail.com>
2022-03-23 10:47:34 +0400

    account page index structure

---
+++ HTML/account/account-index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account</title>
+    
+    <!-- bootstrap link -->
+    <link rel="stylesheet" href="assets/styles/main.css">
+</head>
+<body>
+    <div id="acc-header"></div>
+    <div id="acc-body">
+        <nav id="side-nav">
+            <ul>
+                <li>Profile</li>
+                <li>Orders</li>
+                <li>Wishlist</li>
+                <li>Address</li>
+                <li>Payment</li>
+                <li>Points</li>
+                <li>Log Out</li>
+            </ul>
+        </nav>
+    </div>
