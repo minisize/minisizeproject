9d0fb1e26159279ee2ec7b08e3a37b7747f756de
Sjunsei <71812546+Sjunsei@users.noreply.github.com>
2022-03-29 12:53:10 +0400

    Update Cart
    - Added css link pathway
    - added includes of header & footer

---
+++ HTML/Purchase/cart.php
@@ -0,0 +1,23 @@
+
+    <!-- Default Header Paste-->
+    <?php
+        include("../../includes/sub-header.php");
+    ?>
+        <!-- bootstrap link -->
+        <link rel="stylesheet" href="../../assets/styles/header_footer/header_n_footer.css">
+
+        <!-- bootstrap link -->
+        <link rel="stylesheet" href="../../assets/styles/main.css">
+
+    <!-- Main Content Is here -->
+
+    <main>
+
+
+
+    </main>
+
+    <!-- Default Footer Paste -->
+    <?php
+        include("../../includes/main-footer.php");
+    ?>
