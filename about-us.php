c77fb066807af8bef8d3d12fd1abe89c4db7d639
DavidJos-292710 <71811421+DavidJos-292710@users.noreply.github.com>
2022-03-29 16:19:53 +0400

    Re-arranging the div's into a main div while adding img div

--- about-us.php
+++ about-us.php
@@ -3,9 +3,16 @@
     ?>
 
     <h1> About Minisize </h1>
-    <div class="info">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius autem mollitia quod veritatis fugiat, unde omnis molestiae, ea quisquam adipisci itaque placeat qui perspiciatis in temporibus sit maxime illum commodi? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod rem cupiditate numquam corrupti quam nostrum, minus sit voluptatibus fuga deserunt. Repellendus perferendis nemo sint possimus aliquam reiciendis blanditiis quibusdam aspernatur!
+    <div class="top-info">
+        <div class="us">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius autem mollitia quod veritatis fugiat, unde omnis molestiae, ea quisquam adipisci itaque placeat qui perspiciatis in temporibus sit maxime illum commodi? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod rem cupiditate numquam corrupti quam nostrum, minus sit voluptatibus fuga deserunt. Repellendus perferendis nemo sint possimus aliquam reiciendis blanditiis quibusdam aspernatur!
+        </div>
+        <!-- Will input the finalize image -->
+        <!-- <div class="img">
+            <img>
+        </div> -->
     </div>
+
 
     <div class="quota">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam consequuntur, fugit dolores expedita officia pariatur veniam esse impedit qui dignissimos! Perspiciatis, nam corporis! Reiciendis quae eum at. Porro, voluptas corrupti!
