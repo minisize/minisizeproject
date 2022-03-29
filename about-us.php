f9cc3986a247923b9790fe65cc0482bd2b617889
DavidJos-292710 <71811421+DavidJos-292710@users.noreply.github.com>
2022-03-29 13:10:15 +0400

    Starting to make construction of the about us page

---
+++ about-us.php
@@ -0,0 +1,18 @@
+    <?php
+        include("includes/main-header.php");
+    ?>
+
+    <h1> About Minisize </h1>
+    <div class="info">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius autem mollitia quod veritatis fugiat, unde omnis molestiae, ea quisquam adipisci itaque placeat qui perspiciatis in temporibus sit maxime illum commodi? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod rem cupiditate numquam corrupti quam nostrum, minus sit voluptatibus fuga deserunt. Repellendus perferendis nemo sint possimus aliquam reiciendis blanditiis quibusdam aspernatur!
+    </div>
+
+    <div class="quota">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam consequuntur, fugit dolores expedita officia pariatur veniam esse impedit qui dignissimos! Perspiciatis, nam corporis! Reiciendis quae eum at. Porro, voluptas corrupti!
+    </div>
+
+    <br><br><br><br><br>
+    
+    <?php
+        include("includes/main-footer.php");
+    ?>
