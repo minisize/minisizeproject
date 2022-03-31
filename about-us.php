3d566b4789e83debcad1d82c3bded4c6279f8c97
DavidJos-292710 <71811421+DavidJos-292710@users.noreply.github.com>
2022-03-31 11:54:54 +0400

    Redoing the about us page since I've made a oopies and added a new folder and file which is the scss that is only connected to the about us page.

--- about-us.php
+++ about-us.php
@@ -2,17 +2,85 @@
         include("includes/main-header.php");
     ?>
 
-    <h1> About Minisize </h1>
+    <link rel="stylesheet" href="assets/styles/about_us/about.scss">
+
+    <div class="top-background">
+        <h1> Minisize </h1>
+        <h3> One stop shop for all your skincare needs </h3>
+    </div>
     <div class="top-info">
+        <h2> About Us </h2>
         <div class="us">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius autem mollitia quod veritatis fugiat, unde omnis molestiae, ea quisquam adipisci itaque placeat qui perspiciatis in temporibus sit maxime illum commodi? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod rem cupiditate numquam corrupti quam nostrum, minus sit voluptatibus fuga deserunt. Repellendus perferendis nemo sint possimus aliquam reiciendis blanditiis quibusdam aspernatur!
         </div>
-        <!-- Will input the finalize image -->
-        <!-- <div class="img">
-            <img>
-        </div> -->
     </div>
 
+    <div class="team-mate">
+        <div class="team">
+            <h4>Project Manager</h4>
+            <ul> 
+                <li> Kyla Buarao </li>
+                <li> Kimberly Diaz </li>
+                <li> Allelee Flores </li>
+            </ul>
+        </div>
+        <div class="team">
+            <h4>Developer Team</h4>
+            <ul> 
+                <li> Akilah Lapure </li>
+                <li> Sjunsei Awal </li>
+                <li> David Francisco </li>
+                <li> Kyla Buarao </li>
+            </ul>
+        </div>
+        <div class="team">
+            <h4>UX Team</h4>
+            <ul> 
+                <li> Shayne Fajardo </li>
+                <li> Kimberly Diaz </li>
+                <li> Akilah Lapure </li>
+                <li> Sjunsei Awal </li>
+            </ul>
+        </div>
+        <div class="team">
+            <h4>QA Team</h4>
+            <ul> 
+                <li> Allelee Flores </li>
+                <li> Shayne Fajardo </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="pics">
+        <div class="looks">
+            <img src="#">
+            <p> Akilah Lapure</p>
+        </div>
+        <div class="looks">
+            <img src="#">
+            <p> Allelee Flores</p>
+        </div>
+        <div class="looks">
+            <img src="#">
+            <p> Shayne Fajardo </p>
+        </div>
+        <div class="looks">
+            <img src="#">
+            <p> Kimberly Diaz </p>
+        </div>
+        <div class="looks">
+            <img src="#">
+            <p> Kyla Buarao </p>
+        </div>
+        <div class="looks">
+            <img src="#">
+            <p> David Francisco  </p>
+        </div>
+        <div class="looks">
+            <img src="#">
+            <p> Sjunsei Awal </p>
+        </div>
+    </div>
 
     <div class="quota">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam consequuntur, fugit dolores expedita officia pariatur veniam esse impedit qui dignissimos! Perspiciatis, nam corporis! Reiciendis quae eum at. Porro, voluptas corrupti!
