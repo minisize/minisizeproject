c6beb2f89cc702d6fb4dd5d0ddeec44ea2a2c996
akickandopen <akilah.lapure@gmail.com>
2022-04-06 09:50:54 +0400

    Add ending header tag

--- index.php
+++ index.php
@@ -1,101 +1,101 @@
-    <!-- Default Header Paste-->
-    <?php
-        // include_once '/wamp64/www/phpservers/minisizeproject/includes/main-header.php';
-        include("includes/main-header.php");
-    ?>
+<!-- Default Header Paste-->
+<?php
+    // include_once '/wamp64/www/phpservers/minisizeproject/includes/main-header.php';
+    include("includes/main-header.php");
+?>
 
+</header>
+<!-- Enter Main Content Here-->
 
-    <!-- Enter Main Content Here-->
+<main>
+    <div class="homepage_product_1">
 
-    <main>
-        <div class="homepage_product_1">
-
-            <label for=""> Picture of Hand </label>
-            <div>
-                <h1> MiniSize </h1>
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-            </div>
-
-        </div>
+        <label for=""> Picture of Hand </label>
         <div>
-            <h3> BEST SELLERS </h3>
+            <h1> MiniSize </h1>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+        </div>
 
-            <div class="product_wrapper">
-                <div class="product_item_1">
-                <img src="#">
-                <h6> Item </h6>   
-                </div>
-                <div class="product_item_1">
-                <img src="#">
-                <h6> Item </h6>   
-                </div>
-                <div class="product_item_1">
-                <img src="#">
-                <h6> Item </h6>   
-                </div>
-                <div class="product_item_1">
-                <img src="#">
-                <h6> Item </h6>   
-                </div>
-                <div class="product_item_1">
-                <img src="#">
-                <h6> Item </h6>   
-                </div>
-                
+    </div>
+    <div>
+        <h3> BEST SELLERS </h3>
+
+        <div class="product_wrapper">
+            <div class="product_item_1">
+            <img src="#">
+            <h6> Item </h6>   
+            </div>
+            <div class="product_item_1">
+            <img src="#">
+            <h6> Item </h6>   
+            </div>
+            <div class="product_item_1">
+            <img src="#">
+            <h6> Item </h6>   
+            </div>
+            <div class="product_item_1">
+            <img src="#">
+            <h6> Item </h6>   
+            </div>
+            <div class="product_item_1">
+            <img src="#">
+            <h6> Item </h6>   
             </div>
             
         </div>
-        <div class="main_content">
-            <div class="container1">
-                <img src="#">
-                <div class="container1_subcontent">
-                    <h5> Meet our Bundles! </h5>
-                    <p>Meet our bundle! We provide small set of skincare products for one time use for our customers.</p>
-                    <button> View all </button>
-                </div>
-            </div>
-            <div class="container2">
-                <div class="container2_subcontent">
-                    <h5>Full-size products!</h5>
-                    <p>Full Sized products are available too! make sure to get a subscription in order to get discounts for the produts!</p>
-                    <button> View all </button>
-                </div>
-                <img src="#">
+        
+    </div>
+    <div class="main_content">
+        <div class="container1">
+            <img src="#">
+            <div class="container1_subcontent">
+                <h5> Meet our Bundles! </h5>
+                <p>Meet our bundle! We provide small set of skincare products for one time use for our customers.</p>
+                <button> View all </button>
             </div>
         </div>
-        <div class="sub_content">
-            <h4> Find the right products for your skin type! </h4>
-            <div class="box1">
-                <button> 
-                    <h2> Oily </h2>
-                </button>
-                
+        <div class="container2">
+            <div class="container2_subcontent">
+                <h5>Full-size products!</h5>
+                <p>Full Sized products are available too! make sure to get a subscription in order to get discounts for the produts!</p>
+                <button> View all </button>
             </div>
-            <div class="box2">
-                <button> 
-                    <h2> Dry </h2>
-                </button>
-                
-            </div>
-            <div class="box3">
-                <button> 
-                    <h2> Sensitive </h2>
-                </button>
-                
-            </div>
-            <div class="box4">
-                <button> 
-                    <h2> Combination </h2>
-                </button>
-                
-            </div>
+            <img src="#">
         </div>
-    </main>
+    </div>
+    <div class="sub_content">
+        <h4> Find the right products for your skin type! </h4>
+        <div class="box1">
+            <button> 
+                <h2> Oily </h2>
+            </button>
+            
+        </div>
+        <div class="box2">
+            <button> 
+                <h2> Dry </h2>
+            </button>
+            
+        </div>
+        <div class="box3">
+            <button> 
+                <h2> Sensitive </h2>
+            </button>
+            
+        </div>
+        <div class="box4">
+            <button> 
+                <h2> Combination </h2>
+            </button>
+            
+        </div>
+    </div>
+</main>
 
-    <!-- Default Footer Paste -->
-    <?php
-        include("includes/main-footer.php");
-    ?>
+<!-- Default Footer Paste -->
+<?php
+    include("includes/main-footer.php");
+?>
