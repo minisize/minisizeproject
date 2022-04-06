98af71503c92ea0dad42913c5fce7bd4f26bec48
akickandopen <akilah.lapure@gmail.com>
2022-04-06 20:18:34 +0400

    Add modal dialog

--- sub_pages/popup/registration.php
+++ sub_pages/popup/registration.php
@@ -1,56 +1,67 @@
-<?php require '../../includes/server.php';?>
+<?php 
+    require 'includes/handlers/signup-handler.php';
+    require 'includes/handlers/login-handler.php';
+?>
 
-<!DOCTYPE html>
-<html lang="en">
+<!-- Registration Modal -->
+<div class="modal fade" id="registerModal" tabindex="-1" aria-labelledby="registerModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content" style="height:500px">
+            <div class="modal-header">
+                <h3 class="modal-title" id="registerModalLabel">Hello! Welcome to Minisize</h3>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <ul class="nav nav-tabs nav-fill">
+                    <li class="nav-item">
+                        <a class="nav-link active" data-bs-toggle="tab" href="#signUp">Sign Up</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-bs-toggle="tab" href="#login">Log In</a>
+                    </li>
+                </ul>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+                <div class="tab-content">
+                    <div class="tab-pane active" id="signUp">
+                        <form action="registration.php" method="POST" class="sign-up-form">
+                            <input type="text" class="form-control mt-2" name="sign_up_fname" placeholder="First Name" required>
+                            <input type="text" class="form-control mt-2" name="sign_up_lname" placeholder="Last Name" required>
+                            <input type="email" class="form-control mt-2" name="sign_up_email" placeholder="Email" required>
+                            <input type="password" class="form-control mt-2" name="sign_up_pass" placeholder="Password" required>
+                            <input type="password" class="form-control mt-2" name="sign_up_cpass" placeholder="Confirm Password" required>
+                            <input type="submit" class="btn btn-primary form-control mt-2" name="sign_up_btn" value="Create an account">
 
-    <!-- bootstrap link -->
-    <link rel="stylesheet" href="../../assets/styles/main.css">
+                            <?php
+                                if(empty($errorListRegister)){
+                                    echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                                            <strong>Success!</strong> Sign Up
+                                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                         </div>';
+                                }
+                            ?>
+                        </form>
+                    </div>
+                    <div class="tab-pane" id="login">
+                        <form action="registration.php" method="POST" class="login-form">
+                            <input type="email" class="form-control mt-2" name="login_email" placeholder="Email" required>
+                            <input type="password" class="form-control mt-2" name="login_pass" placeholder="Password" required>
+                            <input type="submit" class="btn btn-primary form-control mt-2" name="login_btn" value="Login">
 
-    <!-- Google Material Icons -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+                            <?php
+                                if(empty($errorListLogin)){
+                                    echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                                            <strong>Success!</strong> Login
+                                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                        </div>';
+                                }
+                            ?>
+                        </form>
+                    </div>
+                </div>
 
-</head>
-
-
-<body>
-
-    <ul class="nav nav-tabs nav-fill">
-        <li class="nav-item">
-            <a class="nav-link active" data-bs-toggle="tab" href="#signUp">Sign Up</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="tab" href="#logIn">Log In</a>
-        </li>
-    </ul>
-
-    <div class="tab-content">
-        <div class="tab-pane active" id="signUp">
-            <form action="registration.php"  method="POST" class="sign-up-form">
-                <input type="text" class="form-control mt-2" name="sign_up_fname" placeholder="First Name" required> 
-                <input type="text" class="form-control mt-2" name="sign_up_lname" placeholder="Last Name" required>
-                <input type="email" class="form-control mt-2" name="sign_up_email" placeholder="Email" required>
-                <input type="password" class="form-control mt-2" name="sign_up_pass" placeholder="Password" required>
-                <input type="password" class="form-control mt-2" name="sign_up_cpass" placeholder="Confirm Password" required>
-                <input type="submit" class="btn btn-primary form-control mt-2" name="sign_up_btn" value="Create an account">
-            </form>
-        </div>
-        <div class="tab-pane" id="logIn">
-            <form action="registration.php"  method="POST" class="login-form">
-                <input type="email" class="form-control mt-2" name="login_email" placeholder="Email" required>
-                <input type="password" class="form-control mt-2" name="login_pass" placeholder="Password" required>
-                <input type="submit" class="btn btn-primary form-control mt-2" name="login_btn" value="Login">
-            </form>
+                <!-- <iframe src="sub_pages/popup/registration.php" id="register_iframe" frameborder="0"
+                    width="100%" height="100%"></iframe> -->
+            </div>
         </div>
     </div>
-
-    <script src="../../node_modules/bootstrap/dist/js/bootstrap.js"></script>
-
-</body>
-
-</html>
+</div>
