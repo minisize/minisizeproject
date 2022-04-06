2d6a4a2e9a648991bce0b132be5d853c6610a11d
akickandopen <akilah.lapure@gmail.com>
2022-04-06 10:25:08 +0400

    Merge branch 'main' into feature_products

---
+++ products.php
@@ -0,0 +1,195 @@
+    <!-- Default Header Paste-->
+    <?php
+        include_once '/wamp64/www/phpservers/minisizeproject/includes/main-header.php';
+    ?>
+
+
+    <!-- Enter Main Content Here-->
+
+    <main>
+        <div class="products-header">
+
+            <div class="container1">
+
+            <h1> Cream </h1>
+            <p>
+            Easy on the eyes. Hydrate and nourish with textures tailored for the area that ages fastest.
+            </p>
+            </div>
+            <div class="container2"> Picture Product </div>
+        </div>
+        <div class="products-maincontent">
+
+            <div class="maincontent-header">
+                <div class="maincontent-filter1">
+
+                <label> Home > Cream </label>
+                </div>
+                <div class="maincontent-filter2">
+                    <div class="filter-section">
+                        <h6> Filter by </h6>
+
+                        <select name="skin-type" id="" >
+                            <option value="select1">Option 1</option>
+                            <option value="select2">Option 1</option>
+                            <option value="select3">Option 1</option>
+                            <option value="select4">Option 1</option>
+                            <option value="select5">Option 1</option>
+                        </select>
+
+                        <select name="ingredient" id="" >
+                            <option value="select1">Option 1</option>
+                            <option value="select2">Option 1</option>
+                            <option value="select3">Option 1</option>
+                            <option value="select4">Option 1</option>
+                            <option value="select5">Option 1</option>
+                        </select>
+
+                        <select name="benefit" id="" >
+                            <option value="select1">Option 1</option>
+                            <option value="select2">Option 1</option>
+                            <option value="select3">Option 1</option>
+                            <option value="select4">Option 1</option>
+                            <option value="select5">Option 1</option>
+                        </select>
+                    </div>
+
+                    <div class="sort-section">
+
+                    <h6> Sort by </h6>
+
+                       <select name="Featured" id="" >
+                            <option value="select1">Option 1</option>
+                            <option value="select2">Option 1</option>
+                            <option value="select3">Option 1</option>
+                            <option value="select4">Option 1</option>
+                            <option value="select5">Option 1</option>
+                        </select>
+
+                    </div>
+                    
+
+                </div>
+
+            </div>
+            <div class="maincontent-container2">
+
+                <div class="product-container">
+                    <label for=""></label><img src="#" alt="">
+                    <div class="product-display">
+                        <h6> Item Name </h6>
+                        <p>subcontent</p>
+                    </div>
+                    <div class="product-price">
+                        <label for=""> 15 AED </label>
+                        <a href=""> 25 reviews </a>
+                    </div>
+                </div>
+                <div class="product-container">
+                    <label for=""></label><img src="#" alt="">
+                    <div class="product-display">
+                        <h6> Item Name </h6>
+                        <p>subcontent</p>
+                    </div>
+                    <div class="product-price">
+                        <label for=""> 15 AED </label>
+                        <a href=""> 25 reviews </a>
+                    </div>
+                </div>
+                <div class="product-container">
+                    <label for=""></label><img src="#" alt="">
+                    <div class="product-display">
+                        <h6> Item Name </h6>
+                        <p>subcontent</p>
+                    </div>
+                    <div class="product-price">
+                        <label for=""> 15 AED </label>
+                        <a href=""> 25 reviews </a>
+                    </div>
+                </div>
+                <div class="product-container">
+                    <label for=""></label><img src="#" alt="">
+                    <div class="product-display">
+                        <h6> Item Name </h6>
+                        <p>subcontent </p>
+                    </div>
+                    <div class="product-price">
+                        <label for=""> 15 AED </label>
+                        <a href=""> 25 reviews </a>
+                    </div>
+                </div>
+                <div class="product-container">
+                    <label for=""></label><img src="#" alt="">
+                    <div class="product-display">
+                        <h6> Item Name </h6>
+                        <p> subcontent </p>
+                    </div>
+
+                    <div class="product-price">
+                        <label for=""> 15 AED </label>
+                        <a href=""> 25 reviews </a>
+                    </div>
+                 </div>
+                <div class="product-container">
+                    <label for=""></label><img src="#" alt="">
+                    <div class="product-display">
+                        <h6> Item Name </h6>
+                        <p>subcontent</p>
+                    </div>
+                    <div class="product-price">
+                        <label for=""> 15 AED </label>
+                        <a href=""> 25 reviews </a>
+                    </div>
+                </div>
+                <div class="product-container">
+                    <label for=""></label><img src="#" alt="">
+                    <div class="product-display">
+                        <h6> Item Name </h6>
+                        <p>subcontent</p>
+                    </div>
+                    <div class="product-price">
+                        <label for=""> 15 AED </label>
+                        <a href=""> 25 reviews </a>
+                    </div>
+                </div>
+                <div class="product-container3">
+                    <label for=""></label><img src="#" alt="">
+                    <div class="product-display">
+                        <h6> Item Name </h6>
+                        <p>subcontent</p>
+                    </div>
+                    <div class="product-price">
+                        <label for=""> 15 AED </label>
+                        <a href=""> 25 reviews </a>
+                    </div>
+                </div>
+                <div class="product-container4">
+                    <label for=""></label><img src="#" alt="">
+                    <div class="product-display">
+                        <h6> Item Name </h6>
+                        <p>subcontent </p>
+                    </div>
+                    <div class="product-price">
+                        <label for=""> 15 AED </label>
+                        <a href=""> 25 reviews </a>
+                    </div>
+                </div>
+                <div class="product-container5">
+                    <label for=""></label><img src="#" alt="">
+                    <div class="product-display">
+                        <h6> Item Name </h6>
+                        <p> subcontent </p>
+                    </div>
+
+                    <div class="product-price">
+                        <label for=""> 15 AED </label>
+                        <a href=""> 25 reviews </a>
+                    </div>
+                </div>
+            </div>
+    </main>
+
+    <!-- Default Footer Paste -->
+    <?php
+        include_once '/wamp64/www/phpservers/minisizeproject/includes/main-footer.php';
+    ?>
