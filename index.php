3794ffd58d2451eb5ed0717b2973e3dde345e25d
Sjunsei <71812546+Sjunsei@users.noreply.github.com>
2022-04-12 01:00:50 +0400

    update
    -updated css

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
 <main class="">
     <div class="container ">
         <div class="row w-100">
-            <div class="col-2 border border-2" > Picture of Hand blablabla </div>
-            <div class="col-10 border border-1">
+            <div class="col-2 " > Picture of Hand blablabla </div>
+            <div class="col-10 ">
                 <h1 class="text-center text-capitalize"> MiniSize </h1>
                 <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
@@ -20,7 +20,7 @@
             </div>
         </div>
     </div>
-    <div class="container">
+    <div class="container p-3">
         <h3 class="row"> BEST SELLERS </h3>
 
         <div class="product_wrapper row">
@@ -50,7 +50,7 @@
     </div>
 
 
-    <div class="main_content container">
+    <div class="main_content container p-3">
         <div class="row">
             <img class="col" src="#">
             <div class="col container1_subcontent">
@@ -68,28 +68,28 @@
             <img class="col" src="#">
         </div>
     </div>
-    <div class="container">
+    <div class="container p-3">
         <h4 class="row"> Find the right products for your skin type! </h4>
         <div class="row">
             <div class="col">
-                <button> 
+                <button class="w-75 p-3"> 
                     <h2> Oily </h2>
                 </button>
             </div>
             <div class="col">
-                <button> 
+                <button class="w-75 p-3"> 
                     <h2> Dry </h2>
                 </button>
             </div>
         </div>
         <div class="row">
             <div class="col">
-                <button> 
+                <button class="w-75 p-3"> 
                     <h2> Sensitive </h2>
                 </button>
             </div>
             <div class="col">
-                <button> 
+                <button class="w-75 p-3"> 
                     <h2> Combination </h2>
                 </button>
             </div>
