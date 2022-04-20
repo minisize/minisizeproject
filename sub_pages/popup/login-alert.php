75832b5f500f199cea50fef1a63858884f9b97e4
akickandopen <akilah.lapure@gmail.com>
2022-04-20 18:36:00 +0400

    Add login alert modal

---
+++ sub_pages/popup/login-alert.php
@@ -0,0 +1,23 @@
+<!-- Registration Modal -->
+<div class="modal fade" id="registerModal" tabindex="-1" aria-labelledby="registerModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-body d-flex flex-wrap justify-content-center">
+
+                <div class="warning">
+                    <img src="assets/images/website/restriction.png" alt="Restricted" width="75">
+                    <h2>Oops!</h2>
+                </div>
+
+                <p class="text-center py-2">Before accessing this feature, you need to be logged in. Create one or use an existing account!</p>
+
+                <div class="buttons">
+                    <a href="products.php" class="btn btn-primary form-control">Log In</a> 
+                    <br>
+                    <a href="registration.php" class="btn form-control">Create an account</a>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
