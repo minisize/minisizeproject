f5146c8044dcd20a2890a04484e1ccd66a15fabc
DavidJos-292710 <71811421+DavidJos-292710@users.noreply.github.com>
2022-04-25 11:22:15 +0400

    Adding the structure of the blog page

---
+++ blog.php
@@ -0,0 +1,34 @@
+<?php
+        include("includes/main-header.php");
+    ?>
+
+</header>
+
+<div id="top">
+    <img src="#">
+    <div class="box-info">
+        <p> Brand - Aug 12 2021 </p>
+        <h4> Get to Know InnisFree and their resources </h4>
+        <p> We offer innovative beauty solutions powered by the finest natural ingredients responsibly sourced from Korea’s pristine Jeju Island. Thanks to its volcanic origin... </p>
+        <button> Read more </button> 
+    </div>
+</div>
+
+<br><br>
+
+<div id="mid">
+    <h1>All Blogs</h1>
+    <div class="filters">
+        <p> Filter by </p>
+        <div class="brands">
+            <button>
+                Brand
+            </button>
+        </div>
+    </div>
+</div>
+
+
+<?php
+        include("includes/main-footer.php");
+    ?>
