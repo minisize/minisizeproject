b040f96c763ac093504ebb0e27fcc1e45e11d69c
akickandopen <akilah.lapure@gmail.com>
2022-04-25 16:49:59 +0400

    Create functions to display product details

--- includes/classes/Product.php
+++ includes/classes/Product.php
@@ -1,66 +1,78 @@
 <?php
     class Product {
-        private $user_obj;
         private $connect;
 
-        public function __construct($connect, $user){
+        public function __construct($connect){
             $this->connect = $connect;
-            $this->user_obj = new User($connect, $user);
         }
 
-        public function loadPage($productType){
-            switch($productType){
-                case "Hyaluronic Acid":
-                    $tab = "By Key Ingredient";
-                    break;
-                case "Niacinamide":
-                    $tab = "By Key Ingredient";
-                    break;
-                case "Vitamin E":
-                    $tab = "By Key Ingredient";
-                    break;
-                case "Antioxidants":
-                    $tab = "By Key Ingredient";
-                    break;
-                case "Salicylic Acid":
-                    $tab = "By Key Ingredient";
-                    break;
-                case "Amino Acids":
-                    $tab = "By Key Ingredient";
-                    break;
-                case "Butylene Glycol":
-                    $tab = "By Key Ingredient";
-                    break;
-                case "Citric Acid":
-                    $tab = "By Key Ingredient";
-                    break;
-                case "Glycerin":
-                    $tab = "By Key Ingredient";
-                    break;
-                case "Hydration":
-                    $tab = "By Concern";
-                    break;
-                case "Pore Solutions":
-                    $tab = "By Concern";
-                    break;
-                case "Troubled Skin":
-                    $tab = "By Concern";
-                    break;
-                case "Dullness & Uneven Skin Tone":
-                    $tab = "By Concern";
-                    break;
-                case "Sensitive Skin":
-                    $tab = "By Concern";
-                    break;
-                case "Age Prevention":
-                    $tab = "By Concern";
-                    break;
-                case "Lifting & Firming":
-                    $tab = "By Concern";
-                    break;
-                default:
-                    break;
+        public function getItemCategory($tab, $itemID) {
+
+            $query = "SELECT * FROM $tab WHERE id='$itemID'";
+            $result = mysqli_query($this->connect, $query);
+            
+            $this->row = mysqli_fetch_array($result);
+
+            return $this->row['name'];
+        }
+
+        public function loadHeader($tab, $itemID){
+
+            $query = "SELECT * FROM $tab WHERE id='$itemID'";
+            $result = mysqli_query($this->connect, $query);
+
+            $row = mysqli_fetch_array($result);
+
+            $name = $row['name'];
+            $description = $row['description'];
+            $image = $row['image'];
+
+            $headerString = "<div class='container'>
+                                <div class='products-header row'>
+                                    <div class='container1 col'>
+                                    <h1> $name </h1>
+                                    <p> $description </p>
+                                    </div>
+                                    <div class='container2 col'> Picture Product </div>
+                                </div>
+                            </div>";
+
+            echo $headerString;
+        }
+
+        public function loadProducts($tab, $itemID){
+
+            if($tab == "categories"){
+                $tab = "category";
             }
+
+            $tabID = $tab . "_id";
+            $productString = "";
+
+            $productDataQuery = mysqli_query($this->connect, "SELECT * FROM products WHERE $tabID='$itemID'");
+
+            while($row = mysqli_fetch_array($productDataQuery)){
+                $id = $row['id'];
+                $name = $row['name'];
+                $mainIngredient = $row['main_ingredient'];
+                $basePrice = $row['base_price'];
+                $numReviews = $row['num_reviews'];
+
+                // $productString .= "$id <br><br> $name <br><br> $description <br><br><br>";
+                $productString .= "<div class='col'>
+                                        <label for=''></label><img src='#' alt=''>
+                                        <div class='product-display'>
+                                            <h6>$name</h6>
+                                            <p>with $mainIngredient</p>
+                                        </div>
+                                        <div class='product-price'>
+                                            <label for=''>$basePrice AED</label>
+                                            <a href=''>$numReviews reviews </a>
+                                        </div>
+                                    </div>";
+            }
+
+            echo $productString;
         }
     }
 ?>
