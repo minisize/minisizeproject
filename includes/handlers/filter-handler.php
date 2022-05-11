bc7311539322b44366d421f0f8f56227c9535fea
akickandopen <akilah.lapure@gmail.com>
2022-05-11 13:34:42 +0400

    Initial commit

---
+++ includes/handlers/filter-handler.php
@@ -0,0 +1,18 @@
+<?php
+    $k = $_POST['id'];
+    $f = $_POST['tab'];
+    $c = $_POST['item'];
+
+    echo $k . "<br>" . $f . "<br>" . $c;
+
+    // if($tab == "categories"){
+    //     $tab = "category";
+    // }
+
+    // $tabID = $tab . "_id";
+    // $productString = "";
+    
+    // if ($k == "Skin Types"){
+    //     sql = "SELECT * FROM products WHERE $tabID='$itemID'";
+    // }
+?>
