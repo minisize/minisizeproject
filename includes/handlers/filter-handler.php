0bcafd7aced11879781986ff79d6efd194e425c6
akickandopen <akilah.lapure@gmail.com>
2022-05-11 21:21:24 +0400

    Sort displayed items by price

--- includes/handlers/filter-handler.php
+++ includes/handlers/filter-handler.php
@@ -1,5 +1,11 @@
 <?php require '../server.php';
+    include("../classes/Product.php");
+    $product_obj = new Product($connect);
 
+    // Variables
+    $filterSkinType = $_POST['skin'];
+    $filterBenefit = $_POST['benefit'];
+    $sortItems = $_POST['sort'];
     $tab = $_POST['tab'];
     $itemID = $_POST['item'];
 
@@ -11,6 +17,27 @@
     $productString = "";
     
     $query = "SELECT * FROM products WHERE $tabID='$itemID'";
+    if(isset($_POST['sort'])){
+
+        if ($sortItems == "PriceHigh") {
+
+            $query.= " ORDER BY base_price DESC";
+
+            if ($filterSkinType == "Skin Types" && $filterBenefit == "Benefits"){
+                echo $product_obj->loadProducts($query);
+            }
+
+        } else if ($sortItems == "PriceLow") {
+
+            $query.= " ORDER BY base_price ASC";
+
+            if ($filterSkinType == "Skin Types" && $filterBenefit == "Benefits"){
+                echo $product_obj->loadProducts($query);
+            }
+
+        }
+    }
+
     $result = mysqli_query($connect, $query);
 
     while($row = mysqli_fetch_array($result)){
@@ -30,59 +57,33 @@
         $concerns = $row['for_skin_concern'];
         $concernArray = explode(",",$concerns);
 
+        $productString = "<div class='col product-display'>
+                            <label for=''></label><img src='$img' alt='product image' class='img-fluid display-item-dimension'>
+                            <div class='product-name'>
+                                <p><strong>$name</strong> <br> with $mainIngredient</p>
+                            </div>
+                            <div class='product-price'>
+                                <label for=''>$basePrice AED</label>
+                                <a href=''>$numReviews reviews </a>
+                            </div>
+                            <div class='overlay-product'></div>
+                            <a href='product-item.php?id=$id' class='product-view btn btn-outline-primary'>View</a>
+                        </div>";
+
         if(isset($_POST['skin']) || isset($_POST['benefit'])){
-            $filterSkinType = $_POST['skin'];
-            $filterBenefit = $_POST['benefit'];
 
             if ((in_array($filterSkinType, $skinTypeArray)) && ($filterBenefit == "Benefits")) {
-
-                $productString .= "<div class='col product-display'>
-                                    <label for=''></label><img src='$img' alt='product image' class='img-fluid display-item-dimension'>
-                                    <div class='product-name'>
-                                        <p><strong>$name</strong> <br> with $mainIngredient</p>
-                                    </div>
-                                    <div class='product-price'>
-                                        <label for=''>$basePrice AED</label>
-                                        <a href=''>$numReviews reviews </a>
-                                    </div>
-                                    <div class='overlay-product'></div>
-                                    <a href='product-item.php?id=$id' class='product-view btn btn-outline-primary'>View</a>
-                                </div>";
+                echo $productString;
 
             } else if ((in_array($filterBenefit, $concernArray)) && ($filterSkinType == "Skin Types")) {
-
-                $productString .= "<div class='col product-display'>
-                                    <label for=''></label><img src='$img' alt='product image' class='img-fluid display-item-dimension'>
-                                    <div class='product-name'>
-                                        <p><strong>$name</strong> <br> with $mainIngredient</p>
-                                    </div>
-                                    <div class='product-price'>
-                                        <label for=''>$basePrice AED</label>
-                                        <a href=''>$numReviews reviews </a>
-                                    </div>
-                                    <div class='overlay-product'></div>
-                                    <a href='product-item.php?id=$id' class='product-view btn btn-outline-primary'>View</a>
-                                </div>";
+                echo $productString;
 
             } else if (in_array($filterSkinType, $skinTypeArray) && in_array($filterBenefit, $concernArray)) {
-
-                $productString .= "<div class='col product-display'>
-                                    <label for=''></label><img src='$img' alt='product image' class='img-fluid display-item-dimension'>
-                                    <div class='product-name'>
-                                        <p><strong>$name</strong> <br> with $mainIngredient</p>
-                                    </div>
-                                    <div class='product-price'>
-                                        <label for=''>$basePrice AED</label>
-                                        <a href=''>$numReviews reviews </a>
-                                    </div>
-                                    <div class='overlay-product'></div>
-                                    <a href='product-item.php?id=$id' class='product-view btn btn-outline-primary'>View</a>
-                                </div>";
-
+                echo $productString;
+                
             }
+            
         }
 
     }
-
-    echo $productString;
 ?>
