fffc6ab602262dd05bc396314ab018f4451b9175
akickandopen <akilah.lapure@gmail.com>
2022-05-14 19:17:19 +0400

    Add category and product data

--- category.php
+++ category.php
@@ -1,6 +1,10 @@
 <!-- Default Header Paste-->
 <?php
     include("includes/main-header.php");
+
+    $categoriesDataQuery = "SELECT * FROM categories";
+    $categoriesData = mysqli_query($connect, $categoriesDataQuery);
+
 ?>
 </header>
 
@@ -12,223 +16,61 @@
             </div>
             <div class="category-nav">
                 <h1 class="row fw-bold">Category</h3>
-                <a href="" class="row">Bundles</a>
-                <a href="" class="row">Moisterizer</a>
-                <a href="" class="row">Toners</a>
-                <a href="" class="row">Serum & Essence</a>
-                <a href="" class="row">Cleanser</a>
-                <a href="" class="row">Masks</a>
+                    <a href="" class="row">Bundles</a>
+                    <a href="" class="row">Moisterizer</a>
+                    <a href="" class="row">Toners</a>
+                    <a href="" class="row">Serum & Essence</a>
+                    <a href="" class="row">Cleanser</a>
+                    <a href="" class="row">Masks</a>
             </div>
         </div>
 
-        
-        <div class="categories">
 
+        <div class="container categories">
             <div class="row py-5 border-bottom">
                 <?php
+                    while($row = mysqli_fetch_array($categoriesData)){
+                        $id = $row["id"];
+                        $name = $row["name"];
+                        $desc = $row["short_description"];
+                        
+                        echo "<div class='container row'>
+                                <div class='col-3'>
+                                    <h3>$name</h3>
+                                    <p>$desc</p>
+                                    <button>View All</button>
+                                </div>";
 
-                //QUERY(IES)
-                $Q = "SELECT * FROM `categories`";
-                $result_FROM_Category = $connect->query($Q);
+                        $productsDataQuery = "SELECT * FROM products WHERE category_id = '$id' LIMIT 3";
+                        $productsData = mysqli_query($connect, $productsDataQuery);
 
-                if ($result_FROM_Category->num_rows > 0) {
+                        while($row = mysqli_fetch_array($productsData)){
+                            $id = $row["id"];
+                            $name = $row["name"];
+                            $brand = $row["brand"];
+                            $basePrice = $row['base_price'];
+                            $numReviews = $row['num_reviews'];
 
-                    //Limit the items to display
-                    $Item_Display_Limit = 1;
+                            $jsonobj = $row["images"];
+                            $obj = json_decode($jsonobj);
+                            $img = $obj->images->image1;
 
-                    while (($row = $result_FROM_Category->fetch_assoc()) && ($Item_Display_Limit === 1)) {
-
-                        //Creation of HTML
-                        echo "
-                        <div class='col-2'>
-                            <h3>" . $row["name"] . "</h3>
-                            <p>" . $row["short_description"] . "</p>
-                            <button> View All </button>
-                        </div>
-                        ";
-
-                        $Item_Display_Limit += 1;
-                    }
-                } else {
-                    echo "0 results";
-                }
-
-                // NEXT DIVISION//
-
-                //Limit the items to display
-                $Item_Display_Limit = 0;
-
-                //QUERY(IES)
-                $Q = "SELECT * FROM `products` WHERE `category_id` = 1";
-                $result_FROM_Category = $connect->query($Q);
-
-                //Gets data from minisize_db
-                if ($result_FROM_Category->num_rows > 0) {
-
-                    while (($row = $result_FROM_Category->fetch_assoc()) && ($Item_Display_Limit <= 2)) {
-
-                        //set $jsonobj to the value of input of the array "images" from $row;
-                        $jsonobj = $row["images"];
-                        //set $obj to the value of a php object converted from the string of $jsonobj
-                        $obj = json_decode($jsonobj);
-                        // Set $img to the value of image1 from images by php object $obj
-                        $img = $obj->images->image1;
-
-                        //Creation of HTML
-                        echo "
-                        <div class='col'>
-                            <div>
-                                <img src='" . $img . "' alt='' class='img-fluid display-item-dimension'>
-                                <h5>" . $row["name"] . "</h5>
-                                <p>" . "By " . $row["brand"] . "</p>
-                            </div>
-                            <div class='d-flex justify-content-between'><label for=''>" . $row["base_price"] . " dhs" . "</label><a href='#'>16 reviews</a></div>
-                        </div>
-                        ";
-                        $Item_Display_Limit += 1;
-                    }
-                } else {
-                    echo "0 results";
-                }
-                ?>
-            </div>
-            <div class="row py-5">
-                <?php
-
-                //QUERY(IES)
-                $Q = "SELECT * FROM `categories` WHERE `id` = 5";
-                $result_FROM_Category = $connect->query($Q);
-
-                if ($result_FROM_Category->num_rows > 0) {
-
-                    //Limit the items to display
-                    $Item_Display_Limit = 1;
-
-                    while (($row = $result_FROM_Category->fetch_assoc()) && ($Item_Display_Limit === 1)) {
-
-                        //Creation of HTML
-                        echo "
-                        <div class='col-2'>
-                            <h3>" . $row["name"] . "</h3>
-                            <p>" . $row["short_description"] . "</p>
-                            <button> View All </button>
-                        </div>
-                        ";
-
-                        $Item_Display_Limit += 1;
-                    }
-                } else {
-                    echo "0 results";
-                }
-
-                // NEXT DIVISION//
-
-                //Limit the items to display
-                $Item_Display_Limit = 0;
-
-                //QUERY(IES)
-                $Q = "SELECT * FROM `products` WHERE `category_id` = 5";
-                $result_FROM_Category = $connect->query($Q);
-
-                //Gets data from minisize_db
-                if ($result_FROM_Category->num_rows > 0) {
-
-                    while (($row = $result_FROM_Category->fetch_assoc()) && ($Item_Display_Limit <= 2)) {
-
-                        //set $jsonobj to the value of input of the array "images" from $row;
-                        $jsonobj = $row["images"];
-                        //set $obj to the value of a php object converted from the string of $jsonobj
-                        $obj = json_decode($jsonobj);
-                        // Set $img to the value of image1 from images by php object $obj
-                        $img = $obj->images->image1;
-
-                        //Creation of HTML
-                        echo "
-                            <div class='col'>
-                                <div>
-                                    <img src='" . $img . "' alt='' class='img-fluid display-item-dimension'>
-                                    <h5>" . $row["name"] . "</h5>
-                                    <p>" . "By " . $row["brand"] . "</p>
-                                </div>
-                                <div class='d-flex justify-content-between'><label for=''>" . $row["base_price"] . " dhs" . "</label><a href='#'>16 reviews</a></div>
-                            </div>
-                            ";
-                        $Item_Display_Limit += 1;
-                    }
-                } else {
-                    echo "0 results";
-                }
-                ?>
-            </div>
-            <div class="row py-5 border-top">
-                <!-- PHP CODE FOR LISTING -->
-                <?php
-
-                //QUERY(IES)
-                $Q = "SELECT * FROM `categories` WHERE `id` = 3";
-                $result_FROM_Category = $connect->query($Q);
-
-                if ($result_FROM_Category->num_rows > 0) {
-
-                    //Limit the items to display
-                    $Item_Display_Limit = 1;
-
-                    while (($row = $result_FROM_Category->fetch_assoc()) && ($Item_Display_Limit === 1)) {
-
-                        //Creation of HTML
-                        echo "
-                        <div class='col-2'>
-                            <h3>" . $row["name"] . "</h3>
-                            <p>" . $row["short_description"] . "</p>
-                            <button> View All </button>
-                        </div>
-                        ";
-
-                        $Item_Display_Limit += 1;
-                    }
-                } else {
-                    echo "0 results";
-                }
-
-                // NEXT DIVISION//
-
-                //Limit the items to display
-                $Item_Display_Limit = 0;
-
-                //QUERY(IES)
-                $Q = "SELECT * FROM `products` WHERE `category_id` = 3";
-                $result_FROM_Category = $connect->query($Q);
-
-                //Gets data from minisize_db
-                if ($result_FROM_Category->num_rows > 0) {
-
-                    while (($row = $result_FROM_Category->fetch_assoc()) && ($Item_Display_Limit <= 2)) {
-
-                        //set $jsonobj to the value of input of the array "images" from $row;
-                        $jsonobj = $row["images"];
-                        //set $obj to the value of a php object converted from the string of $jsonobj
-                        $obj = json_decode($jsonobj);
-                        // Set $img to the value of image1 from images by php object $obj
-                        $img = $obj->images->image1;
-
-                        //Creation of HTML
-                        echo "
-                                <div class='col'>
+                            echo "<div class='col'>
                                     <div>
-                                        <img src='" . $img . "' alt='' class='img-fluid display-item-dimension'>
-                                        <h5>" . $row["name"] . "</h5>
-                                        <p>" . "By " . $row["brand"] . "</p>
+                                        <img src='$img' alt='' class='img-fluid display-item-dimension'>
+                                        <h5>$name</h5>
+                                        <p>By $brand</p>
                                     </div>
-                                    <div class='d-flex justify-content-between'><label for=''>" . $row["base_price"] . " dhs" . "</label><a href='#'>16 reviews</a></div>
-                                </div>
-                                ";
-                        $Item_Display_Limit += 1;
+                                    <div class='d-flex justify-content-between'>$basePrice dhs<a href='#'>$numReviews reviews</a></div>
+                                </div>";
+
+                        }
+
                     }
-                } else {
-                    echo "0 results";
-                }
                 ?>
-            </div>
+
+
+
             </div>
         </div>
     </div>
