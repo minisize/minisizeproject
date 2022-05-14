d53dcfb17bff1f0ff487a732405b28988c1c5a8e
Sjunsei <71812546+Sjunsei@users.noreply.github.com>
2022-05-15 01:17:32 +0400

    main-footer.php
    - added appropriate classes to fix css and html element aspects
    
    - converted buttons into icons
    
    - adjusted text font sizes and font weights according to figma

--- includes/main-footer.php
+++ includes/main-footer.php
@@ -1,24 +1,42 @@
 <footer>
-<div class="footer-wrap side-footer">
-    <h5> Follow Us </h5>
-    <div class="Footer-Sub-Wrap">
-        <label for=""><button>Airplane Icon</button></label>
-        <label for=""><button>Like</button></label>
-        <label for=""><button>Share</button></label>
+    <div class="footer-wrap side-footer ">
+        <h5 class="fw-bold"> Follow Us </h5>
+        <div class="Footer-Sub-Wrap">
+            <label for="">
+                <button class=" border-0 bg-light px-2 mx-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telegram" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z" />
+                    </svg>
+                </button>
+            </label>
+            <label for="">
+                <button class="border-0 bg-light px-2 mx-1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-thumbs-up-fill" viewBox="0 0 16 16">
+                        <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z" />
+                    </svg>
+                </button>
+            </label>
+            <label for="">
+                <button class="border-0 bg-light px-2 mx-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share-fill" viewBox="0 0 16 16">
+                        <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z" />
+                    </svg>
+                </button>
+            </label>
+        </div>
     </div>
-</div>
-<div class="footer-wrap main-footer">
-    <button> SUBSCRIBE FOR MORE REWARDS </button>
-    <label> Website icon </label>
-</div>
-<div class="footer-wrap side-footer">
-    <h5> CONTACT US </h5>
-    <h6> 050 777 7777notarealnumber </h6>
-    <h6> MiniSize@Gmail.com </h6>
-</div>
+    <div class="footer-wrap main-footer">
+        <button class="btn btn-outline-dark border border-1 border-dark rounded-0 px-5 py-2 fw-light "> SUBSCRIBE FOR MORE REWARDS </button>
+        <label> Website icon </label>
+    </div>
+    <div class="footer-wrap side-footer">
+        <h5 class="fw-bold"> CONTACT US </h5>
+        <h6 class="fw-light"> 050 777 7777 </h6>
+        <h6 class="fw-light"> MiniSize@Gmail.com </h6>
+    </div>
 </footer>
 
 <!-- DO NOT REMOVE OR MODIFY PAST THIS -->
 
 </body>
+
 </html>
