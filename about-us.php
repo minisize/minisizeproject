12725e070b333d0d73b6cba75adc2a16b9397d57
DavidJos-292710 <71811421+DavidJos-292710@users.noreply.github.com>
2022-05-18 10:43:33 +0400

    Structuring and styling the about-us page

--- about-us.php
+++ about-us.php
@@ -4,95 +4,120 @@
 
     <link rel="stylesheet" href="assets/styles/about_us/about.css">
 
-    <div class="top-background">
-        <h1 class="about-title"> Minisize </h1>
-        <h3 id="mini-quota"> One stop shop for all your skincare needs </h3>
+    <div class="container">
+        <div class="top-background">
+            <h1 class="about-title"> Minisize </h1>
+            <h3 id="mini-quota"> One stop shop for all your skincare needs </h3>
+        </div>
+        <div class="fade-bg"></div>
     </div>
-    <div class="fade-bg"></div>
-
 </header>
 
-    <div class="top-info">
-        <h2  id="about-title"> About Us </h2>
-        <div class="us">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius autem mollitia quod veritatis fugiat, unde omnis molestiae, ea quisquam adipisci itaque placeat qui perspiciatis in temporibus sit maxime illum commodi? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod rem cupiditate numquam corrupti quam nostrum, minus sit voluptatibus fuga deserunt. Repellendus perferendis nemo sint possimus aliquam reiciendis blanditiis quibusdam aspernatur!
+    <div class="container">
+        <div class="top-info">
+            <h2  id="about-title"> About Us </h2>
+            <div class="us">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius autem mollitia quod veritatis fugiat, unde omnis molestiae, ea quisquam adipisci itaque placeat qui perspiciatis in temporibus sit maxime illum commodi? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod rem cupiditate numquam corrupti quam nostrum, minus sit voluptatibus fuga deserunt. Repellendus perferendis nemo sint possimus aliquam reiciendis blanditiis quibusdam aspernatur!
+            </div>
+        </div>
+
+        <div class="team-mate">
+            <h3  id="about-title"> Meet the team </h3>
+            <div class="team-proto">
+                <div class="team">
+                    <h3>Project Manager</h3>
+                    <h4> Kyla Buarao </h4>
+                    <h4> Kimberly Diaz </h4>
+                    <h4> Allelee Flores </h4>
+                    <!-- <ul> 
+                        <li> Kyla Buarao </li>
+                        <li> Kimberly Diaz </li>
+                        <li> Allelee Flores </li>
+                    </ul> -->
+                </div>
+                <div class="team">
+                    <h3>Developer Team</h3>
+                    <h4> Akilah Lapure </h4>
+                    <h4> Sjunsei Awal </h4>
+                    <h4> David Francisco </h4>
+                    <h4> Kyla Buarao </h4>
+                    <!-- <ul> 
+                        <li> Akilah Lapure </li>
+                        <li> Sjunsei Awal </li>
+                        <li> David Francisco </li>
+                        <li> Kyla Buarao </li>
+                    </ul> -->
+                </div>
+                <div class="team">
+                    <h3>UX Team</h3>
+                    <h4> Shayne Fajardo </h4>
+                    <h4> Kimberly Diaz </h4>
+                    <h4> Akilah Lapure </h4>
+                    <h4> Sjunsei Awal </h4>
+                    <!-- <ul> 
+                        <li> Shayne Fajardo </li>
+                        <li> Kimberly Diaz </li>
+                        <li> Akilah Lapure </li>
+                        <li> Sjunsei Awal </li>
+                    </ul> -->
+                </div>
+                <div class="team">
+                    <h3>Business Team </h3>
+                    <h4> Allelee Flores </h4>
+                    <h4> Shayne Fajardo </h4>
+                    <!-- <ul> 
+                        <li> Allelee Flores </li>
+                        <li> Shayne Fajardo </li>
+                    </ul> -->
+                </div>
+            </div>
+        </div>
+
+        <div class="pics">
+            <div class="looks">
+                <img src="assets\images\website\teams\Akilah.jpg">
+                <h4> Akilah Lapure </h4>
+                <!-- <p> Akilah Lapure</p> -->
+            </div>
+            <div class="looks">
+                <img src="assets\images\website\teams\Allelee.jpg">
+                <h4> Allelee Flores </h4>
+                <!-- <p> Allelee Flores</p> -->
+            </div>
+            <div class="looks">
+                <img  src="assets\images\website\teams\shayne.jpg">
+                <h4> Shayne Fajardo </h4>
+                <!-- <p> Shayne Fajardo </p> -->
+            </div>
+            <div class="looks">
+                <div class="big_img">
+                    <img src="assets\images\website\teams\Kim.jpg">
+                </div>
+                <h4> Kimberly Diaz </h4>
+                <!-- <p> Kimberly Diaz </p> -->
+            </div>
+            <div class="looks">
+                <img src="assets\images\website\teams\Kyla.jpeg">
+                <h4> Kyla Buarao</h4>
+                <!-- <p> Kyla Buarao </p> -->
+            </div>
+            <div class="looks">
+                <img src="assets\images\website\teams\David.jpg">
+                <h4> David Francisco </h4>
+                <!-- <p> David Francisco  </p> -->
+            </div>
+            <div class="looks">
+                <img class="KIM-SJUN" src="assets\images\website\teams\Sjunsei.jpg">
+                <h4> Sjunsei Awal </h4>
+                <!-- <p> Sjunsei Awal </p> -->
+            </div>
+        </div>
+
+        <div class="quota">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam consequuntur, fugit dolores expedita officia pariatur veniam esse impedit qui dignissimos! Perspiciatis, nam corporis! Reiciendis quae eum at. Porro, voluptas corrupti!
         </div>
     </div>
-
-    <div class="team-mate">
-        <h3  id="about-title"> Meet the team </h3>
-        <div class="team">
-            <h4>Project Manager</h4>
-            <ul> 
-                <li> Kyla Buarao </li>
-                <li> Kimberly Diaz </li>
-                <li> Allelee Flores </li>
-            </ul>
-        </div>
-        <div class="team">
-            <h4>Developer Team</h4>
-            <ul> 
-                <li> Akilah Lapure </li>
-                <li> Sjunsei Awal </li>
-                <li> David Francisco </li>
-                <li> Kyla Buarao </li>
-            </ul>
-        </div>
-        <div class="team">
-            <h4>UX Team</h4>
-            <ul> 
-                <li> Shayne Fajardo </li>
-                <li> Kimberly Diaz </li>
-                <li> Akilah Lapure </li>
-                <li> Sjunsei Awal </li>
-            </ul>
-        </div>
-        <div class="team">
-            <h4>QA Team</h4>
-            <ul> 
-                <li> Allelee Flores </li>
-                <li> Shayne Fajardo </li>
-            </ul>
-        </div>
-    </div>
-
-    <div class="pics">
-        <div class="looks">
-            <img src="assets\images\website\teams\Akilah.jpg">
-            <p> Akilah Lapure</p>
-        </div>
-        <div class="looks">
-            <img src="assets\images\website\teams\Allelee.jpg">
-            <p> Allelee Flores</p>
-        </div>
-        <div class="looks">
-            <img  src="assets\images\website\teams\shayne.jpg">
-            <p> Shayne Fajardo </p>
-        </div>
-        <div class="looks">
-            <img src="assets\images\website\teams\Kim.jpg">
-            <p> Kimberly Diaz </p>
-        </div>
-        <div class="looks">
-            <img src="assets\images\website\teams\Kyla.jpg">
-            <p> Kyla Buarao </p>
-        </div>
-        <div class="looks">
-            <img src="assets\images\website\teams\David.jpg">
-            <p> David Francisco  </p>
-        </div>
-        <div class="looks">
-            <img src="assets\images\website\teams\Sjunsei.jpg">
-            <p> Sjunsei Awal </p>
-        </div>
-    </div>
-
-    <div class="quota">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam consequuntur, fugit dolores expedita officia pariatur veniam esse impedit qui dignissimos! Perspiciatis, nam corporis! Reiciendis quae eum at. Porro, voluptas corrupti!
-    </div>
-
-    <br><br><br><br><br>
-    
+        <br><br><br><br><br>  
     <script src="assets/javascript/scroll-anim.js"></script>
 
     <?php
