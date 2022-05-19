6227f9464975f1741f22f27a1f68855246da5b85
akickandopen <akilah.lapure@gmail.com>
2022-05-19 23:21:27 +0400

    Update quantity of items and price

--- includes/handlers/cart-update.php
+++ includes/handlers/cart-update.php
@@ -1,4 +1,22 @@
 <?php
+require '../server.php';
 
+$qty = $_POST['quantity'];
+
+
+if(isset($_POST['update'])){
+    foreach ($_SESSION['cart'] as $key => $value) {
+        if ($value['item_name']==$_POST['item_name']) {
+            $_SESSION['cart'][$key]['quantity']=$qty;
+
+            echo "<script>
+                    alert('Item updated');
+                    window.location.href='../../sub_pages/purchase/cart.php';
+                </script>";
+        }
+        // print_r($key);
+    }
+}
+    
 
 ?>
--- sub_pages/purchase/cart.php
+++ sub_pages/purchase/cart.php
@@ -14,11 +14,12 @@
             <table class="table">
                 <thead>
                     <tr>
-                        <th scope="col">Item</th>
-                        <th scope="col">Cost</th>
-                        <th scope="col">Qty</th>
-                        <th scope="col">Subtotal</th>
-                        <th></th>
+                        <th scope="col"></th>
+                        <th scope="col"><h6 class="fw-bold">Item</h6></th>
+                        <th scope="col"><h6 class="fw-bold">Cost</h6></th>
+                        <th scope="col"><h6 class="fw-bold">Qty</h6></th>
+                        <th scope="col"><h6 class="fw-bold">Subtotal</h6></th>
+                        <th scope="col"></th>
                     </tr>
                 </thead>
 
@@ -28,35 +29,63 @@
 
                 <?php
                     if(isset($_SESSION['cart'])){
-                        $total = 0;
+                        $cartTotal = 0;
                         foreach ($_SESSION['cart'] as $key => $value) {
-                            $total = $total + $value['item_price'];
+                            $name = $value['item_name'];
+                            $itemTotal = $value['item_price'] * $value['quantity'];
+                            $cartTotal = $cartTotal + $itemTotal;
+
+                            $query = mysqli_query($connect, "SELECT images FROM products WHERE name='$name'");
+                            $row = mysqli_fetch_array($query);
+
+                            $jsonobjImg = $row["images"];
+                            $objImg = json_decode($jsonobjImg);
+
+                            $img1 = $objImg->images->image1; 
+
+                            $img = "../../" . $img1;
                         ?>
                             <tr>
-                                <td>
-                                    <p class="fw-light"><?php echo $value['item_size'];?> - <?php echo $value['item_name'];?></p>
+                                <td scope="row">
+                                    <img src="<?php echo $img;?>" alt="" class="col img-fluid cart-image">
                                 </td>
+
                                 <td>
-                                    <p>$<?php echo $value['item_price']?></p>
+                                    <p class="col m-0 p-0"><?php echo $value['item_size'];?> - <?php echo $value['item_name'];?></p>
                                 </td>
+
                                 <td>
-                                    <button class="btn-sm border-0">
-                                        <i class="material-icons">remove</i>
-                                    </button>
-                                    <input type="number" value="<?php echo $value['quantity'];?>" class="d-flex text-center" style="width:3rem;"/>
-                                    <button class="btn-sm border-0">
-                                        <i class="material-icons">add</i>
-                                    </button>
+                                    <p class="m-0 p-0">$<?php echo $value['item_price']?></p>
                                 </td>
+
                                 <td>
-                                    <p>$<?php echo $value['item_price']?></p>
+                                    <form action="../../includes/handlers/cart-update.php" method="POST">
+                                        <div class="input-group mb-3">
+                                            <input type="button" name="update" value="-" class="button-minus btn btn-sm btn-outline-secondary" data-field="quantity" onclick="updateQuantity()">
+                                            <input type="text" id="itemQuantity" name="quantity" step="1" value="<?php echo $value['quantity'];?>" min="1" onchange="updateQuantity()" class="quantity-field w-25 border border-secondary d-flex text-center">
+                                            <input type="hidden" id="itemPrice" name="price" value="<?php echo $value['item_price']?>">
+                                            <input type="button" name="update" value="+" class="button-plus btn btn-sm btn-outline-secondary" data-field="quantity" onclick="updateQuantity()">
+                                        </div>
                                 </td>
+
                                 <td>
+                                    <p class="m-0 p-0">$<?php echo $itemTotal?></p>
+                                </td>
+
+                                <td>
+                                    <!-- <form action="../../includes/handlers/cart-update.php" method="POST"> -->
+                                        <div>
+                                            <button type="submit" name="update" class="btn btn-sm btn-outline-danger text-danger">Update</button>
+                                            <input type="hidden" name="item_name" value="<?php echo $value['item_name']; ?>">
+                                        </div>
+                                    </form>
+
                                     <form action="../../includes/handlers/cart-remove.php" method="POST">
-                                        <button type="submit" name="remove" class="btn-sm btn-danger text-white">Remove</button>
+                                        <button type="submit" name="remove" class="btn btn-sm btn-danger text-white">Remove</button>
                                         <input type="hidden" name="item_name" value="<?php echo $value['item_name']; ?>">
                                     </form>
                                 </td>
+                                
                             </tr>
 
                         <?php
@@ -81,7 +110,7 @@
                     <div>
 
                         <h6>Cart Subtotal</h6>
-                        <p><?php if(isset($_SESSION['cart'])){ echo "$" . $total; } ?></p>
+                        <p><?php if(isset($_SESSION['cart'])){ echo "$" . $cartTotal; } ?></p>
 
                     </div>
 
@@ -98,11 +127,10 @@
             <div>
                 <div>
                     <p class="fw-bold">Estimated total</p>
-                    <p class="fs-1 fw-bold"><?php if(isset($_SESSION['cart'])){ echo "$" . $total; } ?></p>
+                    <p class="fs-1 fw-bold"><?php if(isset($_SESSION['cart'])){ echo "$" . $cartTotal; } ?></p>
                 </div>
                 <div>
-                    <button> View All </button>
-                    <button> Proceed To Checkout</button>
+                    <a href="checkout.php" class="btn btn-primary text-white fw-bold">Proceed to Checkout</a>
                 </div>
             </div>
 
@@ -110,5 +138,54 @@
     </div>
 </div>
 </main>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+    function incrementValue(e) {
+        e.preventDefault();
+        var fieldName = $(e.target).data('field');
+        var parent = $(e.target).closest('div');
+        var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
+
+        if (!isNaN(currentVal)) {
+            parent.find('input[name=' + fieldName + ']').val(currentVal + 1);
+        } else {
+            parent.find('input[name=' + fieldName + ']').val(0);
+        }
+        
+        updateQuantity();
+    }
+
+    function decrementValue(e) {
+        e.preventDefault();
+        var fieldName = $(e.target).data('field');
+        var parent = $(e.target).closest('div');
+        var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
+
+        if (!isNaN(currentVal) && currentVal > 1) {
+            parent.find('input[name=' + fieldName + ']').val(currentVal - 1);
+        } else {
+            parent.find('input[name=' + fieldName + ']').val(1);
+        }
+
+        
+        updateQuantity();
+    }
+
+    $('.input-group').on('click', '.button-plus', function(e) {
+        incrementValue(e);
+    });
+
+    $('.input-group').on('click', '.button-minus', function(e) {
+        decrementValue(e);
+    });
+
+    function updateQuantity() {
+        var itemQuantity = document.getElementById("itemQuantity");
+        var itemNumber = document.getElementById('itemNumber');
+    }
+
+    
+</script>
 
 <?php include("../../includes/sub-footer.php"); ?>
