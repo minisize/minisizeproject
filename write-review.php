d23ac78a014e60dafa7e54e23588efa790d45846
JayceeBuarao <jayceebuarao@gmail.com>
2022-05-21 17:17:30 +0400

    create write-review.php + add structure

---
+++ write-review.php
@@ -0,0 +1,62 @@
+<?php 
+    include("includes/main-header.php");
+
+    // get id
+    if (isset($_GET['id'])) {
+        $itemID = $_GET['id'];
+    }
+
+    $queryProductDetails = mysqli_query($connect, "SELECT * FROM products WHERE id = '$itemID'");
+    $productDetails = mysqli_fetch_array($queryProductDetails);
+?>
+
+<div class="px-5">
+
+    <h2 class='col fw-bold text-darkgreen'>Create a Review</h2>
+    <p class='fs-5'><?php echo $productDetails['name']?> with <?php echo $productDetails['main_ingredient']?></p>
+    <p><?php echo $productDetails['brand']?></p>
+
+    <form action="write-review.php?id=<?php echo $itemID;?>">
+        <div>
+            <h5>Score</h5>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+            </svg>
+        </div>
+        <div>
+            <label for="review-title" class="form-label">Review Title:</label>
+            <input type="text" class="form-control">
+            <label for="review-body" class="form-label">Review:</label>
+            <textarea name="" id="" class="form-control h-25"></textarea>
+        </div>
+        <div>
+            <div>
+                <h5>Add Photos: (optional)</h5>
+                <p>You can add up to 5 Photos</p>
+                <button>Upload Photo</button>
+            </div>
+            <div>
+                <button>Cancel</button>
+                <button>Publish Review</button>
+            </div>
+        </div>
+    </form>
+</div>
+
+
+<!-- Default Footer Paste -->
+<?php
+    include("includes/main-footer.php");
+?>
