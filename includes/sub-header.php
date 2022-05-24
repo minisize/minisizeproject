46316bf4ae2eb7a1f7bed60ffbf3e676e81b71d2
Kimberly Diaz <kimberly.diaz19@bathspa.ac.uk>
2022-05-24 19:58:28 +0400

    Added the final logo design

--- includes/sub-header.php
+++ includes/sub-header.php
@@ -60,7 +60,7 @@
 <body>
     <header class="d-flex flex-wrap align-items-center justify-content-between p-3 border-bottom">
         <a class="">
-            <img src="../../assets/images/website/logo/logo-placeholder.png"
+            <img src="../../assets/images/website/logo/logo.png"
                         alt="Minisize Logo" width="75">
         </a>
 
