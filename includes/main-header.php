6944c6408de6d943c675fc3f5a87f4cbe8ed03ae
Sjunsei <71812546+Sjunsei@users.noreply.github.com>
2022-05-26 12:58:02 +0400

    revert header.php
    - reverting to previous commits in order to create items in a single line

--- includes/main-header.php
+++ includes/main-header.php
@@ -69,8 +69,67 @@ $blog_obj = new Blog($connect);
 
     <header class="top-background">
 
+<<<<<<< HEAD
         <header class="d-flex flex-wrap align-items-center justify-content-between p-3">
             <i class="bi bi-gear-fill fs-5 d-flex align-items-center col-1"></i>
+=======
+    <header class="d-flex flex-wrap align-items-center justify-content-between p-3">
+        <i class="bi bi-gear-fill fs-5 d-flex align-items-center col-1"></i>
+            
+        <ul class="main-nav nav col-8 justify-content-around align-items-center">
+            <li class="nav-item py-4"><a href="index.php">Home</a></li>
+            <li class="nav-item nav-item-dropdown py-4"><a href="#">Shop</a>
+                <section class="dropdown-menu mt-4">
+                    <ul>
+                        <li class="nav-item nav-item-dropdown"><a href="category.php" class="dropdown-item py-2">Category</a>
+                            <section class="dropdown-menu sub-menu">
+                                <ul>
+                                    <li><a href="products.php?category_id=1" class="dropdown-item py-4">Bundles</a></li>
+                                    <li><a href="products.php?category_id=2" class="dropdown-item py-4">Cleanser</a></li>
+                                    <li><a href="products.php?category_id=3" class="dropdown-item py-4">Toner</a></li>
+                                    <li><a href="products.php?category_id=4" class="dropdown-item py-4">Serum & Essence</a></li>
+                                    <li><a href="products.php?category_id=5" class="dropdown-item py-4">Moisturizer</a></li>
+                                    <li><a href="products.php?category_id=6" class="dropdown-item py-4">Masks</a></li>
+                                </ul>
+                            </section>
+                        </li>
+                        <li class="nav-item nav-item-dropdown"><a href="#" class="dropdown-item py-2">By Key Ingredient</a>
+                            <section class="dropdown-menu sub-menu">
+                                <ul>
+                                    <li><a href="products.php?key_ingredient_id=1" class="dropdown-item py-4">Hyaluronic Acid</a></li>
+                                    <li><a href="products.php?key_ingredient_id=2" class="dropdown-item py-4">Niacinamide</a></li>
+                                    <li><a href="products.php?key_ingredient_id=3" class="dropdown-item py-4">Vitamin E</a></li>
+                                    <li><a href="products.php?key_ingredient_id=4" class="dropdown-item py-4">Antioxidants</a></li>
+                                    <li><a href="products.php?key_ingredient_id=5" class="dropdown-item py-4">Salicylic Acid</a></li>
+                                    <li><a href="products.php?key_ingredient_id=6" class="dropdown-item py-4">Amino Acids</a></li>
+                                    <li><a href="products.php?key_ingredient_id=7" class="dropdown-item py-4">Butylene Glycol</a></li>
+                                    <li><a href="products.php?key_ingredient_id=8" class="dropdown-item py-4">Citric Acid</a></li>
+                                    <li><a href="products.php?key_ingredient_id=9" class="dropdown-item py-4">Glycerin</a></li>
+                                </ul>
+                            </section>
+                        </li>
+                        <li class="nav-item nav-item-dropdown"><a href="#" class="dropdown-item py-2">By Concern</a>
+                            <section class="dropdown-menu sub-menu">
+                                <ul>
+                                    <li><a href="products.php?skin_concern_id=1" class="dropdown-item py-4">Hydration</a></li>
+                                    <li><a href="products.php?skin_concern_id=2" class="dropdown-item py-4">Pore Solutions</a></li>
+                                    <li><a href="products.php?skin_concern_id=3" class="dropdown-item py-4">Troubled Skin</a></li>
+                                    <li><a href="products.php?skin_concern_id=4" class="dropdown-item py-4">Dullness & Uneven Skin Tone</a></li>
+                                    <li><a href="products.php?skin_concern_id=5" class="dropdown-item py-4">Sensitive Skin</a></li>
+                                    <li><a href="products.php?skin_concern_id=6" class="dropdown-item py-4">Age Prevention</a></li>
+                                    <li><a href="products.php?skin_concern_id=7" class="dropdown-item py-4">Lifting & Firming</a></li>
+                                </ul>
+                            </section>
+                        </li>
+                    </ul>
+                </section>
+            </li>
+            <li class="nav-item"><a href="index.php"><img src="assets/images/website/logo/logo.png"
+                        alt="Minisize Logo" width="75"></a></li>
+            <li class="nav-item"><a href="blog-list.php">Blogs</a></li>
+            <li class="nav-item"><a href="about-us.php">About</a></li>
+        </ul>
+>>>>>>> parent of 131e4d0 (update header.css)
 
             <ul class="main-nav nav col-8 justify-content-around align-items-center">
                 <li class="main-nav-item nav-item py-4"><a href="index.php">Home</a></li>
